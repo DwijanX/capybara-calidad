7b87fd5b7c4ce3e5c6b4d7999e68f7b5c62cde4f
Ogreaggressive <nicolascari.rz@gmail.com>
2023-12-01 13:54:22 -0400

    finalizando hooks

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -21,6 +21,39 @@ After do
   duration = (end_time - @start_time).to_i
   puts "Scenario took #{duration} seconds to execute."
 end
+
+#probamos de nuevo los escenarios que fueron mas propensos a fallar sin motivo
+After('@homeLink') do |scenario|
+  if scenario.failed?
+    puts "Scenario failed. Retrying once..."
+    retry_count = 1
+
+    begin
+      case scenario.name
+      when /Check reference link "Salon Travel"/
+        step 'I click the "Salon Travel" link'
+        step 'I should be at the trip advisor page'
+      when /Navigate to the Business Travel @ About.com link/
+        step 'I click the "Business Travel @ About.com" link'
+        step 'I should be redirected to the Business Travel page'
+      end
+    rescue => e
+      if retry_count > 0
+        puts "Retry attempt #{retry_count} after failure."
+        retry_count -= 1
+        retry
+      else
+        puts "Retry limit reached. Scenario failed."
+      end
+    end
+  end
+end
+
+#el default max wait time es 2, lo subimos a 5 por si existe conexion mala de internet
+Before do
+  Capybara.default_max_wait_time = 5
+end
+
 #After('') do |scenario|
 
 #  logout_xpath = '/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[2]/td/table/tbody/tr/td[1]/a'
